6fb18ac9731249137977d430aba436e277f1ca68
Fatima <fatimasultanova.909@gmail.com>
2023-09-21 19:08:34 +0400

    Game Service

--- src/service/GameService.java
+++ src/service/GameService.java
@@ -1,4 +1,36 @@
 package service;
 
-public class GameService {
+import model.Word;
+import service.inter.FileServicInter;
+import service.inter.GameServiceInter;
+
+public class GameService implements GameServiceInter {
+    private final FileServicInter fileServic;
+
+    public GameService(){
+        fileServic = new FileService();
+    }
+    @Override
+    public void start() {
+        Word[] words = fileServic.readFile();
+        int point=0;
+
+        System.out.println("------------>Start<------------");
+        for (int i = 0; i <10 ; i++) {
+
+
+        }
+
+
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void see() {
+
+    }
 }
--- src/service/inter/GameServiceInter.java
+++ src/service/inter/GameServiceInter.java
@@ -1,4 +1,7 @@
 package service.inter;
 
 public interface GameServiceInter {
+    public void start();
+    public void add();
+    public void see();
 }
